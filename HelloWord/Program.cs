a7f621f87436d0e28cfa98f4af906c0001b75b78
darkish101 <hosam702@hotmail.com>
2022-11-04 13:12:21 +0300

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
